6f4ac11d2c24c9a758c5051ed295e03183684cd7
EnricoSaggiorato <saggiorato.enrico@gmail.com>
2021-03-06 15:11:06 +0100

    animated menu

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,26 +2,42 @@ import * as React from "react";
 import { Link } from "gatsby";
 import { StaticImage } from "gatsby-plugin-image";
 import * as styles from "./Header.module.css";
+import Menu from "../components/Menu";
+import { motion } from "framer-motion";
 
-const Header = ({ onMenuClick }) => {
+const Header = () => {
+  const [menuOpen, setMenuOpen] = React.useState(false);
+
+  function handleMenuClick() {
+    setMenuOpen(!menuOpen);
+  }
+
   return (
+    <>
     <nav className={styles.navbar}>
       <div className={styles.menu} >
         <Link to="/"><h1 className={styles.title}>IN A PREVIOUS LIFE</h1></Link>
         <div role="button" className={styles.hamburger} tabIndex={0}
-        onClick={onMenuClick} onKeyDown={() => {}}>
-          <div className={styles.hamburgerLine}></div>
+        onClick={handleMenuClick} onKeyDown={() => {}}>
+          <div className={ `${styles.hamburgerLine} ${menuOpen ? styles.rotate : ""}`  }></div>
         </div>
       </div>
       <div className={styles.social}>
-        <StaticImage src="../images/instagram.png" alt="Instagram" />
-        <StaticImage src="../images/spotify.png" alt="Spotify" />
-        <StaticImage src="../images/tiktok.png" alt="Tiktok" />
-        <StaticImage src="../images/facebook.png" alt="Facebook" />
-        <StaticImage src="../images/youtube.png" alt="Youtube" />
+        <StaticImage src="../images/instagram.svg" alt="Instagram" />
+        <StaticImage src="../images/spotify.svg" alt="Spotify" />
+        <StaticImage src="../images/tiktok.svg" alt="Tiktok" />
+        <StaticImage src="../images/facebook.svg" alt="Facebook" />
+        <StaticImage src="../images/youtube.svg" alt="Youtube" />
       </div>
-      <div className={styles.divider} />
+      <motion.div 
+        initial={{ scale: menuOpen ? 1 : 0 }}
+        animate={{ scale: menuOpen ? 0 : 1}}
+        transition={{ duration: .6 }} 
+        className={styles.divider} /> 
     </nav>
+    
+    <Menu isOpen={menuOpen}/>
+    </>
   );
 }
 
--- src/components/Menu.js
+++ src/components/Menu.js
@@ -1,16 +1,28 @@
 import * as React from "react";
-/* import { Link } from "gatsby"; */
+import { Link } from "gatsby";
 import * as styles from "./Menu.module.css";
+import { motion, AnimatePresence } from "framer-motion";
 
-const Menu = () => {
+const Menu = ({ isOpen }) => {
+
   return (
-		<ul className={styles.menu}>
-			<li className={styles.menuItem}>MEDIA</li>
-			<li className={styles.menuItem}>ABOUT US</li>
-			<li className={styles.menuItem}>MERCHANDISE</li>
-			<li className={styles.menuItem}>CONTACT US</li>
-		</ul>
+		<AnimatePresence>
+		{ isOpen && (
+			<motion.div 
+				initial={{ opacity: 0, scaleY: 0 }}
+				animate={{ opacity: 1, scaleY: 1 }}
+				exit={{ opacity: 0, scaleY: 0 }}
+				transition={{ duration: .6 }}>
+				<ul className={styles.menu}>
+					<Link to="/media"><li className={styles.menuItem}>MEDIA</li></Link>
+					<Link to="/about"><li className={styles.menuItem}>ABOUT US</li></Link>
+					<Link to="/merch"><li className={styles.menuItem}>MERCHANDISE</li></Link>
+					<Link to="/contact"><li className={styles.menuItem}>CONTACT US</li></Link>
+				</ul>
+			</motion.div>
+			) }
+		</AnimatePresence>
 	);
 }
 
-export default Menu;
+export default Menu; 
