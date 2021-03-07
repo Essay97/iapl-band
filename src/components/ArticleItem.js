2026e0d87fe5cf6813e281ec258fe65665c1b650
EnricoSaggiorato <saggiorato.enrico@gmail.com>
2021-03-07 20:35:03 +0100

    made about page

---
+++ src/components/ArticleItem.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import * as styles from './ArticleItem.module.css';
+import { GatsbyImage } from 'gatsby-plugin-image';
+
+const ArticleItem = ({ textFirst, title, image }) => {
+  return (
+    <div className={styles.card}>
+      { textFirst && (
+        <h4 className={styles.title}>{title}</h4>
+      ) }
+      <GatsbyImage image={image} alt={`${title} - thumbnail`} />
+      { !textFirst && (
+        <h4 className={styles.title}>{title}</h4>
+      ) }
+    </div>
+  )
+}
+
+export default ArticleItem
